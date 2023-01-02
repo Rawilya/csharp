002ec999e4dd06003b83c554da7f1e68dc8474ad
Raw <rawilya_ayupova@mail.ru>
2023-01-02 20:38:18 +0300

    добавление всех файлов

--- Program.cs
+++ Program.cs
@@ -11,50 +11,7 @@ namespace project
    {
      
      public static void Main()
-    {  
-      
-      
-      System.Console.Write("ВВедите имя:");
-      string ? name = Console.ReadLine()??"";
-      System.Console.Write("ВВедите логин:");
-      string ? login = Console.ReadLine()?? "";
-      System.Console.Write("ВВедите возраст:");
-      int age = Convert.ToInt32(Console.ReadLine());
-      System.Console.Write("Введите хобби: ");
-      string hobby = Console.ReadLine()??"";
-      string[] hobbiArr = hobby.Split(",");
-      List<string> hobbies = hobbiArr.ToList();
-
-      
-
-      XmlSerializer xml = new XmlSerializer(typeof(Users));
-      using(FileStream file= new FileStream("object.xml", FileMode.OpenOrCreate)){
-        Users Yuliya = new Users(name, login,age,hobbies);
-        xml.Serialize(file,Yuliya);
-        System.Console.WriteLine("Сериализация выполнена успешно");
-      }
-      
-    
-      using(FileStream file= new FileStream("object.xml", FileMode.OpenOrCreate)){
-       
-        xml.Deserialize(file);
-        System.Console.WriteLine("Дериализация выполнена успешно");
-        
-      }
-      Users user = new Users();
-        user.GetInfo();
-      
-       
-     
-      
-      
-     
-     
-      
-      
-      
-
-       
+    {
     }
   }      
        
